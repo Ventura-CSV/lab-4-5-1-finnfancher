f3d5d64c088b495740d801aa9b14caf7a5c954da
Finn Fancher <fancherfinn@gmail.com>
2024-06-28 12:01:21 -0700

    update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     Code Your Program here
     ########################################
     """
+    total = 0
+    numbers = [0] * 5
+    for i in range(5):
+        numbers.append(random.randint(0, 100))
+        total += numbers[i]
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
